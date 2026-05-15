4c24d415c0a2c1c1e75c5bb4aee0e18aead0ad90
Sheena Grace <ehnzzzo@gmail.com>
2026-05-15 17:15:43 +0800

    Redirect homepage to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Micro Lending System</h1>
-
-      <p>Admin • Supervisor • Collector • Borrower Platform</p>
-
-      <div style={{ marginTop: 20 }}>
-        <a href="/login">Login</a>
-        <br />
-        <a href="/register">Register</a>
-      </div>
-    </main>
-  );
+  redirect("/login")
 }
